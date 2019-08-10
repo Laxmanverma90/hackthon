f44d9ebf9006f496ee7c6e01edd4c8889010ffbb
Laxman Verma <laxman.verma90@gmail.com>
2019-08-10 17:16:14 +0530

    Update BenchController.java

--- src/main/java/com/dna/hackthon/controller/BenchController.java
+++ src/main/java/com/dna/hackthon/controller/BenchController.java
@@ -2,7 +2,7 @@ package com.dna.hackthon.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin
 @RestController
 public class BenchController {
 
