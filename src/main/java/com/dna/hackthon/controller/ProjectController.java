acaffcb070d4388a13cac907cf09cdd9710fe0dd
Laxman Verma <laxman.verma90@gmail.com>
2019-08-10 17:12:14 +0530

    Update ProjectController.java

--- src/main/java/com/dna/hackthon/controller/ProjectController.java
+++ src/main/java/com/dna/hackthon/controller/ProjectController.java
@@ -13,7 +13,7 @@ import com.dna.hackthon.entity.Demand;
 import com.dna.hackthon.service.ProjectService;
 
 @RestController
- @CrossOrigin(origins = "http://10.117.189.112:8080")
+ @CrossOrigin
 public class ProjectController {
 	@Autowired
     private ProjectService projectService;
