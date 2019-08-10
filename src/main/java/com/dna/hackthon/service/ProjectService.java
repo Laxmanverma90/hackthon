6995dea8d05d0631ac3372b889327fd16358dc7f
Laxman Verma <laxman.verma90@gmail.com>
2019-08-10 15:14:01 +0530

    fix for create demand

--- src/main/java/com/dna/hackthon/service/ProjectService.java
+++ src/main/java/com/dna/hackthon/service/ProjectService.java
@@ -1,8 +1,25 @@
 package com.dna.hackthon.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.dna.hackthon.entity.Demand;
+import com.dna.hackthon.repository.DemandRepositroy;
 
 @Service
 public class ProjectService {
+	@Autowired
+	private DemandRepositroy demRepositroy;
+	
+	public String addDemand(Demand demand) {
+	 
+		String retuenObj = null;
+		 if(demRepositroy.save(demand)!= null) {
+			 retuenObj = "Demand created Success";
+		 } else {
+			 retuenObj ="";
+		 }
+		 return retuenObj;
+	}
 
 }
