3f3418e93d7a9b71191c4c768c481199e0a1f21f
Laxman Verma <laxman.verma90@gmail.com>
2019-08-10 16:33:35 +0530

    code commit

--- src/main/java/com/dna/hackthon/controller/BenchController.java
+++ src/main/java/com/dna/hackthon/controller/BenchController.java
@@ -3,7 +3,6 @@ package com.dna.hackthon.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/supplymanagement")
 @RestController
 public class BenchController {
 
