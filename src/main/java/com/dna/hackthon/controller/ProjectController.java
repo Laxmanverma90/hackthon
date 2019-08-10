c1cdcc4c2d53d966f92e8bc660a1f7d55fab5ef7
Laxman Verma <laxman.verma90@gmail.com>
2019-08-10 16:32:58 +0530

    code commit

--- src/main/java/com/dna/hackthon/controller/ProjectController.java
+++ src/main/java/com/dna/hackthon/controller/ProjectController.java
@@ -6,20 +6,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dna.hackthon.entity.Demand;
 import com.dna.hackthon.service.ProjectService;
 
-@RequestMapping("/demandmanagement")
 @RestController
 public class ProjectController {
 	@Autowired
     private ProjectService projectService;
 	
-	@PostMapping("/demand")
+	@RequestMapping(method = RequestMethod.POST,value = "/demand")
 	public ResponseEntity<String> addDemand(@RequestBody Demand demand) {
 		
 		return new ResponseEntity<String>(projectService.addDemand(demand), HttpStatus.CREATED);
 	}
+	
+	
 }
